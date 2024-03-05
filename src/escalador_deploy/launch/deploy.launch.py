462efc2ceba7654d26b1cc0a15a8d0dcf161e564
Brenno Domingues <brenno.domingues@sc.senai.br>
2024-03-05 15:54:24 -0300

    add service and deploy

---
+++ src/escalador_deploy/launch/deploy.launch.py
@@ -0,0 +1,58 @@
+from launch_ros.actions import Node
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    share_dir = get_package_share_directory('escalador_deploy')
+
+    rviz_config_file = os.path.join(share_dir, 'config', 'display.rviz')
+
+    gui_arg = DeclareLaunchArgument(
+        name='gui',
+        default_value='True'
+    )
+
+    show_gui = LaunchConfiguration('gui')
+
+  
+
+    joint_state_publisher_node = Node(
+        condition=UnlessCondition(show_gui),
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher'
+    )
+
+    joint_state_publisher_gui_node = Node(
+        condition=IfCondition(show_gui),
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher'
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config_file],
+        output='screen'
+    )
+
+    service = Node(
+            package='service_change_base',
+            executable='service',
+            name='servicecaller'
+        )
+
+
+    return LaunchDescription([
+        service,
+        gui_arg,
+        joint_state_publisher_node,
+        joint_state_publisher_gui_node,
+        rviz_node
+    ])
---
+++ src/service_change_base/service_change_base/changeclient.py
@@ -0,0 +1,46 @@
+from escalador_interfaces.srv import ChangeBase                                                           # CHANGE
+import sys
+import rclpy
+from rclpy.node import Node
+
+
+class ChangeBaseClient(Node):
+
+    def __init__(self):
+        super().__init__('chbase_client_async')
+        self.cli = self.create_client(ChangeBase, 'ChangeBase')       # CHANGE
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.req = ChangeBase.Request()                                   # CHANGE
+
+    def send_request(self):
+        self.req.change = sys.argv[1] == 'True'
+        self.future = self.cli.call_async(self.req)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    chbase_client = ChangeBaseClient()
+    chbase_client.send_request()
+
+    while rclpy.ok():
+        rclpy.spin_once(chbase_client)
+        if chbase_client.future.done():
+            try:
+                response = chbase_client.future.result()
+            except Exception as e:
+                chbase_client.get_logger().info(
+                    'Service call failed %r' % (e,))
+            else:
+                chbase_client.get_logger().info(
+                    'Value %s for Base %s' %                                # CHANGE
+                    (chbase_client.req.change, response.basename))  # CHANGE
+            break
+
+    chbase_client.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
