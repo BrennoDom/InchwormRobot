8b55d90336a287d3b0ee1a3075454162124191bc
Brenno Domingues <brenno.domingues@sc.senai.br>
2024-03-05 16:00:40 -0300

    add service and deploy

--- src/service_change_base/service_change_base/changeserver.py
+++ src/service_change_base/service_change_base/changeserver.py
@@ -1,25 +1,8 @@
-from launch_ros.actions import Node
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
-from launch.conditions import IfCondition, UnlessCondition
-from ament_index_python.packages import get_package_share_directory
-import xacro
-import os
 from escalador_interfaces.srv import ChangeBase                                                           # CHANGE
 
 import rclpy
 from rclpy.node import Node
 
-share_dir_1 = get_package_share_directory('escalador_description_serial_1')
-share_dir_2 = get_package_share_directory('escalador_description_serial_2')
-
-xacro_file_1 = os.path.join(share_dir_1, 'urdf', 'escalador_serial_1.xacro')
-xacro_file_2 = os.path.join(share_dir_2, 'urdf', 'escalador_serial_2.xacro')
-robot_description_config_1 = xacro.process_file(xacro_file_1)
-robot_description_config_2 = xacro.process_file(xacro_file_2)
-robot_urdf_1 = robot_description_config_1.toxml()
-robot_urdf_2 = robot_description_config_2.toxml()
 
 class ChangeBaseService(Node):
 
@@ -31,8 +14,6 @@ class ChangeBaseService(Node):
 
         if request.change is False:
             response.basename = 'Base 0'
-            
-
         else:
             response.basename = 'Base 1'
         self.get_logger().info('Incoming request\n%s' % (request.change))  # CHANGE
